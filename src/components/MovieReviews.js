265e0f3cd06686cadca129567c54c6b4512c14d5
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-15 14:39:39 -0500

    commit tree 721abec0ea3c9789327a18f0949af293680cd2c8

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
-// Code MovieReviews Here
+
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+  return(
+    <div className="review-list">
+      {reviews.map(m => {
+        return (
+        <div className="review" id={m.display_title}>
+          <h2>{m.display_title}</h2>
+          <h4>By: {m.byline}</h4>
+          <p>{m.summary_short}</p>
+        </div>
+        )
+      })
+      }
+    </div>
+  )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,43 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+
+  state = {
+    reviews: [],
+    searchTerm: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+
+  fetchReviews = () => {
+    fetch(URL + `&query=${this.state.searchTerm}`)
+    .then(res => res.json())
+    .then(({results}) => {
+      this.setState({
+        reviews: results
+      })
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.fetchReviews()
+  }
+
+  render() {
+    return(
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="search">Search:</label>
+          <input type="text" id="search" value={this.state.searchTerm} onChange={this.handleChange}></input>
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
